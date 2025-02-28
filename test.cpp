61e9a5849af7a6206e323648e48295d774a1f973
ChristopherCalderon <62729165+ChristopherCalderon@users.noreply.github.com>
2025-02-27 18:34:22 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp" // Incluir catch.hpp desde la carpeta include/catch2
+#include "catch2/catch_test_macros.hpp" // Incluir el archivo principal de Catch2 v3.x
 #include <sstream>
 #include <string>
 #include "triangulo.cpp" // Incluye el archivo del estudiante
