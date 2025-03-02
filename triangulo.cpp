3fc68e1e72d9e38a2fef76bea063a949b7b3061c
ChristopherCalderon <62729165+ChristopherCalderon@users.noreply.github.com>
2025-03-01 22:28:12 -0600

    Update triangulo.cpp

--- triangulo.cpp
+++ triangulo.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 using namespace std;
 
+// Función para imprimir un triángulo rectángulo de asteriscos
 void imprimirTriangulo(int altura) {
-    // Implementar funcion aqui
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl; // Salto de línea después de cada fila
+    }
 }
 
 int main() {
     int a, b, c;
+    // Leer las alturas de los tres triángulos
     cin >> a >> b >> c;
+
+    // Imprimir los tres triángulos
     imprimirTriangulo(a);
     imprimirTriangulo(b);
     imprimirTriangulo(c);
+
     return 0;
 }
